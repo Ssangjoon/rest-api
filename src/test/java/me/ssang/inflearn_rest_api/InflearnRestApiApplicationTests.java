9a49f2fcb8070d64cd7349c91b1acc30a0e022f0
SangJoon Lee <qkqh34k@gmail.com>
2024-06-18 21:56:25 +0900

    프로젝트 생성

---
+++ src/test/java/me/ssang/inflearn_rest_api/InflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssang.inflearn_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
